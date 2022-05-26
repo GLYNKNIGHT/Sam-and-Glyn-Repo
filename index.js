0579238982b5d5c7d0c527bdd7c05534d6dc8e6d
glynk <glyn_knight@hotmail.com>
2022-05-26 12:22:14 +0100

    ENV files

---
+++ index.js
@@ -0,0 +1,14 @@
+// npm init *
+// install dotenv  *
+// create a .env file *
+// create some env * 
+// switch to es6 by adding type modules in package.json *
+// import dotenv in to index.js *
+// call variables in index.js*
+// run is script 
+//require('dotenv').config()
+
+//import 'dotenv/config'
+
+console.log(process.env.PORT)
+console.log(process.env.SECRET)
